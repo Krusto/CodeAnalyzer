d2efa08eda85af7ef0332130cc81dfe2f94695cd
Krusto Stoyanov <k.stoianov2@gmail.com>
2024-10-23 22:38:30 +0300

    moved everything into CodeAnalyzer struct

--- src/main.zig
+++ src/main.zig
@@ -15,8 +15,10 @@ pub const FunctionImplementationInfo = struct {
     line_number: usize,
     offset: usize,
     body: stdtypes.String,
-    pub fn init(line_number: usize, offset: usize, body: stdtypes.String) FunctionImplementationInfo {
-        return FunctionImplementationInfo{ .line_number = line_number, .offset = offset, .body = body };
+    start_line: usize,
+    end_line: usize,
+    pub fn init(line_number: usize, offset: usize, body: stdtypes.String, start_line: usize, end_line: usize) FunctionImplementationInfo {
+        return FunctionImplementationInfo{ .line_number = line_number, .offset = offset, .body = body, .start_line = start_line, .end_line = end_line };
     }
 };
 
@@ -36,136 +38,152 @@ pub const CodeAnalyzer = struct {
 
         return CodeAnalyzer{ .fileData = fileData, .functionDeclarations = functionDeclarations, .functionImplementations = functionImplementations, .functionDeclarationInfos = functionDeclarationInfos, .functionImplementationInfos = functionImplementationInfos, .allocator = allocator };
     }
-    pub fn deinit(self: @This()) void {
+
+    pub fn deinit(self: *@This()) void {
         self.fileData.deinit();
     }
+    
+    pub fn addDeclaration(self: *@This(), declaration: []const u8, line_number: usize, offset: usize) !void {
+        try self.functionDeclarations.append(stdtypes.StringUnmanaged.init(declaration));
+        try self.functionDeclarationInfos.put(declaration, FunctionDeclarationInfo.init(line_number, offset));
+    }
+    
+    pub fn getDeclarationLength(code: []const u8, offset: usize) usize {
+        var length: usize = 0;
+        while (code[offset + length] != ';' and code[offset + length] != '{') {
+            length += 1;
+        }
+        return length;
+    }
+    
+    pub fn extractFunctionDeclarations(self: *@This()) !void {
+        var line_number: usize = 1;
+        var previous_token: []const u8 = undefined;
+        previous_token.len = 0;
+        while (self.fileData.offset < self.fileData.code.slice.len) {
+            if (self.fileData.current() == '\n') {
+                line_number += 1;
+                self.fileData.offset += 1;
+            }
+
+            const token = utils.getToken(self.fileData.code.slice, self.fileData.offset);
+
+            var stripedToken = stdtypes.StringUnmanaged{ .slice = undefined };
+            if (token.len == 0)
+                break;
+
+            if (token[0] != ' ') {
+                stripedToken = stdtypes.StringUnmanaged.init(try utils.skipWhiteSpace(token));
+
+                if (utils.startsWith(stripedToken.slice, "static") == true and
+                    utils.startsWith(previous_token, "inline") == false)
+                {
+                    const length = CodeAnalyzer.getDeclarationLength(self.fileData.code.slice, self.fileData.offset);
+                    const declaration = try utils.skipWhiteSpace(self.fileData.currentRange(length));
+                    try addDeclaration(self, declaration, line_number, self.fileData.offset);
+                    self.fileData.offset += self.fileData.currentRange(length).len;
+                } else {
+                    self.fileData.offset += token.len;
+                }
+                previous_token = stripedToken.slice;
+            } else {
+                self.fileData.offset += 1;
+            }
+        }
+        self.fileData.offset = 0;
+    }
+
+    pub fn addImplementation(self: *@This(), declaration: []const u8, line_number: usize, offset: usize) !void {
+        const striped_line = try utils.skipWhiteSpace(declaration);
+
+        try self.functionImplementations.append(stdtypes.StringUnmanaged.init(striped_line));
+        try self.functionImplementationInfos.put(striped_line, FunctionImplementationInfo.init(line_number, offset, stdtypes.String.emptyInit(), 0, 0));
+    }
+
+    pub fn extractFunctionBody(self: *@This(), declaration: stdtypes.StringUnmanaged) !void {
+        const striped_line = try utils.skipWhiteSpace(declaration.slice);
+        const info: FunctionImplementationInfo = self.functionImplementationInfos.get(striped_line).?;
+
+        var brace_count: usize = 1;
+        var i = info.offset + 1;
+
+        while (brace_count > 0 and i < self.fileData.code.slice.len) {
+            const current_char = self.fileData.code.at(i);
+            if (current_char == '{') {
+                brace_count += 1;
+            } else if (current_char == '}') {
+                brace_count -= 1;
+            }
+            i += 1;
+        }
+
+        const body = self.fileData.code.slice[info.offset..i];
+        const value_ptr = self.functionImplementationInfos.getPtr(striped_line).?;
+        value_ptr.*.body = stdtypes.String.init(self.allocator, body);
+    }
+
+    pub fn extractFunctionImplementations(self : *@This()) !void {
+        var line_number: usize = 1;
+
+        while (self.fileData.offset < self.fileData.code.slice.len) {
+            if (self.fileData.current() == '\n') {
+                line_number += 1;
+                self.fileData.offset += 1;
+            }
+
+            const token = utils.getToken(self.fileData.code.slice, self.fileData.offset);
+            var stripedToken = stdtypes.StringUnmanaged{ .slice = undefined };
+            if (token.len == 0)
+                break;
+            if (token[0] != ' ') {
+                stripedToken = stdtypes.StringUnmanaged.init(try utils.skipWhiteSpace(token));
+
+                if (utils.startsWith(stripedToken.slice, "inline") == true) {
+                    const length = CodeAnalyzer.getDeclarationLength(self.fileData.code.slice, self.fileData.offset);
+                    const declaration = try utils.skipWhiteSpace(self.fileData.currentRange(length));
+                    self.fileData.offset += self.fileData.currentRange(length).len;
+                    try self.addImplementation(declaration[6..], line_number, self.fileData.offset);
+                } else {
+                    self.fileData.offset += token.len;
+                }
+            } else {
+                self.fileData.offset += 1;
+            }
+        }
+        self.fileData.offset = 0;
+
+        for (self.functionImplementations.items) |declaration| {
+            try self.extractFunctionBody(declaration);
+        }
+    }
+
+    pub fn extractFunctions(self: *@This()) !void {
+        try extractFunctionDeclarations(self);
+        try extractFunctionImplementations(self);
+    }
+
+    pub fn checkFunctionOrder(self: *@This()) void {
+        std.log.info("Checking Function Order...", .{});
+        if (self.functionDeclarations.items.len != self.functionImplementations.items.len) {
+            std.log.err("\nError: Function declarations and definitions do not match.", .{});
+            return;
+        }
+    }
+
 };
-pub fn addDeclaration(analyzer: *CodeAnalyzer, declaration: []const u8, line_number: usize, offset: usize) !void {
-    try analyzer.functionDeclarations.append(stdtypes.StringUnmanaged.init(declaration));
-    try analyzer.functionDeclarationInfos.put(declaration, FunctionDeclarationInfo.init(line_number, offset));
-}
-pub fn getDeclarationLength(code: []const u8, offset: usize) usize {
-    var length: usize = 0;
-    while (code[offset + length] != ';' and code[offset + length] != '{') {
-        length += 1;
-    }
-    return length;
-}
-pub fn extractFunctionDeclarations(analyzer: *CodeAnalyzer) !void {
-    var line_number: usize = 1;
-    var previous_token: []const u8 = undefined;
-    previous_token.len = 0;
-    while (analyzer.fileData.offset < analyzer.fileData.code.slice.len) {
-        if (analyzer.fileData.current() == '\n') {
-            line_number += 1;
-            analyzer.fileData.offset += 1;
-        }
 
-        const token = utils.getToken(analyzer.fileData.code.slice, analyzer.fileData.offset);
-
-        var stripedToken = stdtypes.StringUnmanaged{ .slice = undefined };
-        if (token.len == 0)
-            break;
-
-        if (token[0] != ' ') {
-            stripedToken = stdtypes.StringUnmanaged.init(try utils.skipWhiteSpace(token));
-
-            if (utils.startsWith(stripedToken.slice, "static") == true and
-                utils.startsWith(previous_token, "inline") == false)
-            {
-                const length = getDeclarationLength(analyzer.fileData.code.slice, analyzer.fileData.offset);
-                const declaration = try utils.skipWhiteSpace(analyzer.fileData.currentRange(length));
-                try addDeclaration(analyzer, declaration, line_number, analyzer.fileData.offset);
-                analyzer.fileData.offset += analyzer.fileData.currentRange(length).len;
-            } else {
-                analyzer.fileData.offset += token.len;
-            }
-            previous_token = stripedToken.slice;
-        } else {
-            analyzer.fileData.offset += 1;
-        }
-    }
-    analyzer.fileData.offset = 0;
-}
-pub fn addImplementation(analyzer: *CodeAnalyzer, declaration: []const u8, line_number: usize, offset: usize) !void {
-    const striped_line = try utils.skipWhiteSpace(declaration);
-
-    try analyzer.functionImplementations.append(stdtypes.StringUnmanaged.init(striped_line));
-    try analyzer.functionImplementationInfos.put(striped_line, FunctionImplementationInfo.init(line_number, offset, stdtypes.String.emptyInit()));
-}
-pub fn extractFunctionBody(analyzer: *CodeAnalyzer, declaration: stdtypes.StringUnmanaged) !void {
-    const striped_line = try utils.skipWhiteSpace(declaration.slice);
-    const info: FunctionImplementationInfo = analyzer.functionImplementationInfos.get(striped_line).?;
-
-    var brace_count: usize = 1;
-    var i = info.offset + 1;
-
-    while (brace_count > 0 and i < analyzer.fileData.code.slice.len) {
-        const current_char = analyzer.fileData.code.at(i);
-        if (current_char == '{') {
-            brace_count += 1;
-        } else if (current_char == '}') {
-            brace_count -= 1;
-        }
-        i += 1;
-    }
-
-    const body = analyzer.fileData.code.slice[info.offset..i];
-    const value_ptr = analyzer.functionImplementationInfos.getPtr(striped_line).?;
-    value_ptr.*.body = stdtypes.String.init(analyzer.allocator, body);
-}
-
-pub fn extractFunctionImplementations(analyzer: *CodeAnalyzer) !void {
-    var line_number: usize = 1;
-
-    while (analyzer.fileData.offset < analyzer.fileData.code.slice.len) {
-        if (analyzer.fileData.current() == '\n') {
-            line_number += 1;
-            analyzer.fileData.offset += 1;
-        }
-
-        const token = utils.getToken(analyzer.fileData.code.slice, analyzer.fileData.offset);
-        var stripedToken = stdtypes.StringUnmanaged{ .slice = undefined };
-        if (token.len == 0)
-            break;
-        if (token[0] != ' ') {
-            stripedToken = stdtypes.StringUnmanaged.init(try utils.skipWhiteSpace(token));
-
-            if (utils.startsWith(stripedToken.slice, "inline") == true) {
-                const length = getDeclarationLength(analyzer.fileData.code.slice, analyzer.fileData.offset);
-                const declaration = try utils.skipWhiteSpace(analyzer.fileData.currentRange(length));
-                analyzer.fileData.offset += analyzer.fileData.currentRange(length).len;
-                try addImplementation(analyzer, declaration[6..], line_number, analyzer.fileData.offset);
-            } else {
-                analyzer.fileData.offset += token.len;
-            }
-        } else {
-            analyzer.fileData.offset += 1;
-        }
-    }
-    analyzer.fileData.offset = 0;
-
-    for (analyzer.functionImplementations.items) |declaration| {
-        try extractFunctionBody(analyzer, declaration);
-    }
-}
-
-pub fn extractFunctions(analyzer: *CodeAnalyzer) !void {
-    try extractFunctionDeclarations(analyzer);
-    try extractFunctionImplementations(analyzer);
-}
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
-    const file_path = stdtypes.StringUnmanaged.init("./test_file.h");
+    var analyzer = try CodeAnalyzer.init(allocator, .{.slice="./test_file.h"});
 
-    var analyzer = try CodeAnalyzer.init(allocator, file_path);
-
-    try extractFunctions(&analyzer);
+    try analyzer.extractFunctions();
 
     std.log.debug("{d} declarations!", .{analyzer.functionDeclarations.items.len});
     std.log.debug("{d} implementations!", .{analyzer.functionImplementations.items.len});
+
+    analyzer.checkFunctionOrder();
 
     defer analyzer.deinit();
 }
